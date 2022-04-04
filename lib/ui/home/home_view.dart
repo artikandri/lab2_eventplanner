ec4bb1ba4d7e42956b899f72a253a1641f7684c6
FlutLab.io <>
2022-04-04 23:49:44 +0000

    Autosave

--- lib/ui/home/home_view.dart
+++ lib/ui/home/home_view.dart
@@ -3,11 +3,11 @@ import 'package:eventplanner/ui/home/home_viewmodel.dart';
 class HomeView {
   HomeViewModel _viewModel;
 
-  void setEventsData(List events) {
-    _viewModel.events = events;
-  }
+  // void setEventsData(List events) {
+  //   _viewModel.events = events;
+  // }
 
-  getEventsData() {
-    return _viewModel.events;
-  }
+  // getEventsData() {
+  //   return _viewModel.events;
+  // }
 }
