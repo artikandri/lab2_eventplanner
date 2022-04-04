55a0c3e9e25a8bbe9e7c8ae263181abba3651c96
FlutLab.io <>
2022-04-04 17:48:47 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:eventplanner/theme/colors/light_colors.dart';
 import 'package:eventplanner/ui/home/presenter/home_presenter.dart';
 import 'package:eventplanner/ui/home/views/home_component.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    systemNavigationBarColor: LightColors.kLightYellow, // navigation bar color
+    statusBarColor: Color(0xffffb969), // status bar color
+  ));
+
+  return runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
