e3e5b8dc2f3cc32b14b4ca32be0ffd9f60be4842
FlutLab.io <>
2022-04-04 23:30:37 +0000

    Autosave

--- lib/utils/shared_prefs_helper.dart
+++ lib/utils/shared_prefs_helper.dart
@@ -2,9 +2,9 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 
 class SharedPrefsHelper {
-  SharedPrefsHelper._privateConstructor();
+  // SharedPrefsHelper._privateConstructor();
 
-  static final SharedPrefsHelper instance = SharedPrefsHelper._privateConstructor();
+  // static final SharedPrefsHelper instance = SharedPrefsHelper._privateConstructor();
 
   setListData(String key, List value) async {
     SharedPreferences myPrefs = await SharedPreferences.getInstance();
