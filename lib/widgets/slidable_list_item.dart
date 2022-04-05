baf0d7aed94a21544232f143f78e3cf05ec11efd
FlutLab.io <>
2022-04-05 22:26:23 +0000

    Autosave

--- lib/widgets/slidable_list_item.dart
+++ lib/widgets/slidable_list_item.dart
@@ -3,8 +3,8 @@ import 'package:flutter_slidable/flutter_slidable.dart';
 
 class SlidableListItem extends StatelessWidget {
   final Widget child;
-  final Function onMarkAsReadButtonClicked;
-  final Function onDeleteButtonClicked;
+  final VoidCallback onMarkAsReadButtonClicked;
+  final VoidCallback onDeleteButtonClicked;
 
   SlidableListItem({this.child, this.onMarkAsReadButtonClicked, this.onDeleteButtonClicked});
 
@@ -12,33 +12,33 @@ class SlidableListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Slidable(
       key: const ValueKey(0),
-      // startActionPane: ActionPane(
-      //   motion: const ScrollMotion(),
-      //   dismissible: DismissiblePane(onDismissed: () {}),
-      //   children: [
-      //     SlidableAction(
-      //       onPressed: onMarkAsReadButtonClicked,
-      //       backgroundColor: Color(0xFF21B7CA),
-      //       foregroundColor: Colors.white,
-      //       icon: Icons.share,
-      //       label: 'Mark as done',
-      //     ),
-      //   ],
-      // ),
-      // endActionPane: ActionPane(
-      //   motion: ScrollMotion(),
-      //   children: [
-      //     SlidableAction(
-      //       // An action can be bigger than the others.
-      //       flex: 2,
-      //       onPressed: onDeleteButtonClicked,
-      //       backgroundColor: Color(0xFF7BC043),
-      //       foregroundColor: Colors.white,
-      //       icon: Icons.delete,
-      //       label: 'Remove',
-      //     ),
-      //   ],
-      // ),
+      startActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        dismissible: DismissiblePane(onDismissed: () {}),
+        children: [
+          SlidableAction(
+            onPressed: onMarkAsReadButtonClicked,
+            backgroundColor: Color(0xFF21B7CA),
+            foregroundColor: Colors.white,
+            icon: Icons.share,
+            label: 'Mark as done',
+          ),
+        ],
+      ),
+      endActionPane: ActionPane(
+        motion: ScrollMotion(),
+        children: [
+          SlidableAction(
+            // An action can be bigger than the others.
+            flex: 2,
+            onPressed: onDeleteButtonClicked,
+            backgroundColor: Color(0xFF7BC043),
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Remove',
+          ),
+        ],
+      ),
       child: child,
     );
   }
