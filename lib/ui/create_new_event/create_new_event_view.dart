af3a701cef7ef8eb844a66b3c405145f457d0ead
FlutLab.io <>
2022-04-05 21:10:39 +0000

    Autosave

--- lib/ui/create_new_event/create_new_event_view.dart
+++ lib/ui/create_new_event/create_new_event_view.dart
@@ -3,4 +3,6 @@ import 'package:eventplanner/ui/create_new_event/create_new_event_viewmodel.dart
 class CreateNewEventView {
   // set fungsi yg mau dipakai buat set state di component
   // nanti di component fungsinya di-override
+
+  CreateNewEventView();
 }
