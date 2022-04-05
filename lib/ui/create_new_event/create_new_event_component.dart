39c693f45fc5aa2093d2148a80020324aa83a062
FlutLab.io <>
2022-04-05 23:06:44 +0000

    Autosave

--- lib/ui/create_new_event/create_new_event_component.dart
+++ lib/ui/create_new_event/create_new_event_component.dart
@@ -42,7 +42,7 @@ class _CreateNewEventPageState extends State<CreateNewEventPage> implements Crea
             child: Column(children: <Widget>[
           TopContainer(
             padding: EdgeInsets.fromLTRB(20, 20, 20, 40),
-            width: double.infinity,
+            width: width,
             child: Column(
               children: <Widget>[
                 MyBackButton(),
@@ -60,109 +60,25 @@ class _CreateNewEventPageState extends State<CreateNewEventPage> implements Crea
                 ),
                 SizedBox(height: 20),
                 Container(
-                    width: width,
                     child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Textbox(label: 'Title'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.end,
                       children: <Widget>[
-                        Textbox(label: 'Title'),
-                        Center(
-                          child: new Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-                            FormBuilder(
-                              key: _formKey,
-                              child: Column(
-                                children: <Widget>[
-                                  Expanded(
-                                      child: Container(
-                                    child: FormBuilderTextField(
-                                      name: 'age',
-                                      decoration: InputDecoration(
-                                        labelText: 'This value is passed along to the [Text.maxLines] attribute of the [Text] widget used to display the hint text.',
-                                      ),
-                                      // onChanged: _onChanged,
-                                      // valueTransformer: (text) => num.tryParse(text),
-                                      validator: FormBuilderValidators.compose([
-                                        FormBuilderValidators.required(context),
-                                        FormBuilderValidators.numeric(context),
-                                        FormBuilderValidators.max(context, 70),
-                                      ]),
-                                      keyboardType: TextInputType.number,
-                                    ),
-                                  )),
-                                  FormBuilderChoiceChip(
-                                    name: 'status',
-                                    decoration: InputDecoration(
-                                      labelText: 'Select event status',
-                                    ),
-                                    options: [
-                                      FormBuilderFieldOption(value: 0, child: Text('To Do')),
-                                      FormBuilderFieldOption(value: 1, child: Text('In Progress')),
-                                      FormBuilderFieldOption(value: 2, child: Text('Done')),
-                                    ],
-                                  ),
-                                  FormBuilderDateTimePicker(
-                                    name: 'date',
-                                    // onChanged: _onChanged,
-                                    inputType: InputType.time,
-                                    decoration: InputDecoration(
-                                      labelText: 'Appointment Time',
-                                    ),
-                                    initialTime: TimeOfDay(hour: 8, minute: 0),
-                                    // initialValue: DateTime.now(),
-                                    // enabled: true,
-                                  ),
-                                  FormBuilderDateRangePicker(
-                                    name: 'date_range',
-                                    firstDate: DateTime(1970),
-                                    lastDate: DateTime(2030),
-                                    format: DateFormat('yyyy-MM-dd'),
-                                    // onChanged: _onChanged,
-                                    decoration: InputDecoration(
-                                      labelText: 'Date Range',
-                                      helperText: 'Helper text',
-                                      hintText: 'Hint text',
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Row(
-                              children: <Widget>[
-                                Expanded(
-                                  child: MaterialButton(
-                                    color: Theme.of(context).colorScheme.secondary,
-                                    child: Text(
-                                      "Submit",
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                    onPressed: () {
-                                      _formKey.currentState.save();
-                                      if (_formKey.currentState.validate()) {
-                                        print(_formKey.currentState.value);
-                                      } else {
-                                        print("validation failed");
-                                      }
-                                    },
-                                  ),
-                                ),
-                                SizedBox(width: 20),
-                                Expanded(
-                                  child: MaterialButton(
-                                    color: Theme.of(context).colorScheme.secondary,
-                                    child: Text(
-                                      "Reset",
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                    onPressed: () {
-                                      _formKey.currentState.reset();
-                                    },
-                                  ),
-                                ),
-                              ],
-                            )
-                          ]),
+                        Expanded(
+                          child: Textbox(
+                            label: 'Date',
+                            icon: downwardIcon,
+                          ),
                         ),
+                        // calendarIcon(),
                       ],
-                    ))
+                    )
+                  ],
+                ))
               ],
             ),
           ),
@@ -199,7 +115,7 @@ class _CreateNewEventPageState extends State<CreateNewEventPage> implements Crea
           )),
           Container(
             height: 80,
-            // width: width,
+            width: width,
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
@@ -210,7 +126,7 @@ class _CreateNewEventPageState extends State<CreateNewEventPage> implements Crea
                   ),
                   alignment: Alignment.center,
                   margin: EdgeInsets.fromLTRB(20, 10, 20, 20),
-                  // width: width - 40,
+                  width: width - 40,
                   decoration: BoxDecoration(
                     color: AppColors.kBlue,
                     borderRadius: BorderRadius.circular(30),
