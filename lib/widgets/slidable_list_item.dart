d8e75f3a2e49da96a42747a40f63c524eb50bc52
FlutLab.io <>
2022-04-05 22:25:22 +0000

    Autosave

--- lib/widgets/slidable_list_item.dart
+++ lib/widgets/slidable_list_item.dart
@@ -10,37 +10,36 @@ class SlidableListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
-    // return Slidable(
-    //   key: const ValueKey(0),
-    //   startActionPane: ActionPane(
-    //     motion: const ScrollMotion(),
-    //     dismissible: DismissiblePane(onDismissed: () {}),
-    //     children: [
-    //       SlidableAction(
-    //         onPressed: onMarkAsReadButtonClicked,
-    //         backgroundColor: Color(0xFF21B7CA),
-    //         foregroundColor: Colors.white,
-    //         icon: Icons.share,
-    //         label: 'Mark as done',
-    //       ),
-    //     ],
-    //   ),
-    //   endActionPane: ActionPane(
-    //     motion: ScrollMotion(),
-    //     children: [
-    //       SlidableAction(
-    //         // An action can be bigger than the others.
-    //         flex: 2,
-    //         onPressed: onDeleteButtonClicked,
-    //         backgroundColor: Color(0xFF7BC043),
-    //         foregroundColor: Colors.white,
-    //         icon: Icons.delete,
-    //         label: 'Remove',
-    //       ),
-    //     ],
-    //   ),
-    //   child: child,
-    // );
+    return Slidable(
+      key: const ValueKey(0),
+      // startActionPane: ActionPane(
+      //   motion: const ScrollMotion(),
+      //   dismissible: DismissiblePane(onDismissed: () {}),
+      //   children: [
+      //     SlidableAction(
+      //       onPressed: onMarkAsReadButtonClicked,
+      //       backgroundColor: Color(0xFF21B7CA),
+      //       foregroundColor: Colors.white,
+      //       icon: Icons.share,
+      //       label: 'Mark as done',
+      //     ),
+      //   ],
+      // ),
+      // endActionPane: ActionPane(
+      //   motion: ScrollMotion(),
+      //   children: [
+      //     SlidableAction(
+      //       // An action can be bigger than the others.
+      //       flex: 2,
+      //       onPressed: onDeleteButtonClicked,
+      //       backgroundColor: Color(0xFF7BC043),
+      //       foregroundColor: Colors.white,
+      //       icon: Icons.delete,
+      //       label: 'Remove',
+      //     ),
+      //   ],
+      // ),
+      child: child,
+    );
   }
 }
