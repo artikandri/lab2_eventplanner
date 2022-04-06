83f25fdfc72e0a48ce58ba9e8e63dee06dcfdfbd
FlutLab.io <>
2022-04-06 20:07:16 +0000

    Autosave

--- lib/ui/home_2/home_component.dart
+++ lib/ui/home_2/home_component.dart
@@ -201,7 +201,7 @@ class _HomePageState extends State<HomePage> implements HomeView {
                                                                     onTap: () {
                                                                       this.widget.presenter.onEventListItemTapped(context, _events[index]);
                                                                     },
-                                                                    child: EventListItem(title: _events[index]['name'], date: _events[index]['date'], subtitle: getStatusLabelFromValue(_events[index]['status']), eventIcon: getStatusIconFromValue(_events[index]['status'])),
+                                                                    child: EventListItem(title: _events[index]['name'], date: _events[index]['date'], subtitle: getStatusLabelFromValue(_events[index]['status']), eventIcon: getStatusIconFromValue(_events[index]['status']), eventColor: getStatusColorFromValue(_events[index]['status'])),
                                                                   )),
                                                               onMarkAsReadButtonClicked: () {
                                                                 this.widget.presenter.onMarkAsReadButtonClicked(context, _events[index]);
