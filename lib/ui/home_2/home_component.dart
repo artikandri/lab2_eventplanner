7c77c21800e9809b48c63eb6ee06251e0104e553
FlutLab.io <>
2022-04-06 16:16:27 +0000

    Autosave

--- lib/ui/home_2/home_component.dart
+++ lib/ui/home_2/home_component.dart
@@ -31,6 +31,7 @@ class _HomePageState extends State<HomePage> implements HomeView {
     this.widget.presenter.homeView = this;
     _futureEvents = this.widget.presenter.getEventListData();
     _futureEvents.then((value) {
+      print(value);
       setEventsData(value);
     });
   }
