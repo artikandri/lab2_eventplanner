3bd5b881bc42d20e85a136afd890b5d46d8d52f4
FlutLab.io <>
2022-04-06 23:46:06 +0000

    Autosave

--- lib/widgets/event_card.dart
+++ lib/widgets/event_card.dart
@@ -24,7 +24,7 @@ class EventCard extends StatelessWidget {
       ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
