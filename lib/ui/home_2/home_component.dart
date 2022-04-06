faa6b8c2689317ea13eed743921a58b973c36c43
FlutLab.io <>
2022-04-06 23:50:40 +0000

    Autosave

--- lib/ui/home_2/home_component.dart
+++ lib/ui/home_2/home_component.dart
@@ -196,6 +196,7 @@ class _HomePageState extends State<HomePage> implements HomeView {
                                                           String eventSubtitle = getStatusLabelFromValue(_events[index]['status']);
                                                           double eventOpacity = _events[index]['isRead'] ? .5 : 1;
                                                           return Container(
+                                                            height: 50,
                                                             child: SlidableListItem(
                                                               child: Container(
                                                                   width: double.infinity,
