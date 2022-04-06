ed914403529d1a56ebda129beb7ff2cf9b2014ff
FlutLab.io <>
2022-04-06 19:55:31 +0000

    Autosave

--- lib/ui/event_detail/event_detail_component.dart
+++ lib/ui/event_detail/event_detail_component.dart
@@ -47,7 +47,7 @@ class _EventDetailPageState extends State<EventDetailPage> implements EventDetai
           TopContainer(
             padding: EdgeInsets.fromLTRB(20, 20, 20, 40),
             width: width,
-            containerColor: getStatusColorFromValue(eventData.toMap()['status']),
+            containerColor: getStatusColorFromValue(_eventData['status']),
             child: Column(
               children: <Widget>[
                 MyBackButton(),
