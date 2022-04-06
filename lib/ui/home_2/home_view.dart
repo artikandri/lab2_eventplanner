9623b17480ec1b45e48f90067e9664dca16d81a1
FlutLab.io <>
2022-04-06 11:11:41 +0000

    Autosave

--- lib/ui/home_2/home_view.dart
+++ lib/ui/home_2/home_view.dart
@@ -2,5 +2,5 @@ import 'package:eventplanner/ui/home_2/home_viewmodel.dart';
 
 class HomeView {
   onLoadEvents(List events) {}
-  String getTodoEventsTagline() {}
+  String getEventsTaglineByStatus(int eventStatus) {}
 }
