130f418216fce13a47c607aabdddba8f675732dc
FlutLab.io <>
2022-04-06 23:46:37 +0000

    Autosave

--- lib/widgets/event_card.dart
+++ lib/widgets/event_card.dart
@@ -15,7 +15,7 @@ class EventCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 200,
+      width: 100,
       margin: EdgeInsets.symmetric(vertical: AppSpacings.defaultSpacing),
       padding: EdgeInsets.all(AppSpacings.defaultSpacing * 2),
       decoration: BoxDecoration(
