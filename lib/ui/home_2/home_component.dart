d8b92d01c227c3baca39b7e2003e2addae0ad54e
FlutLab.io <>
2022-04-06 13:47:28 +0000

    Autosave

--- lib/ui/home_2/home_component.dart
+++ lib/ui/home_2/home_component.dart
@@ -37,9 +37,8 @@ class _HomePageState extends State<HomePage> implements HomeView {
   }
 
   setEventsData() {
-    if(_events.length > 0) {
-    print(_events[0]['datetime'].isAfter(DateTime.now().subtract(Duration(days: 1))))
-
+    if (_events.length > 0) {
+      print(_events[0]['datetime'].isAfter(DateTime.now().subtract(Duration(days: 1))));
     }
     _todayEvents = _events.where((_event) => _event['datetime'].isAfter(DateTime.now().subtract(Duration(days: 1)))).toList();
   }
