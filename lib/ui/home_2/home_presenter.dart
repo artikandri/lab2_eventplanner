8154a067d973ff3081f56007ec44391649057b62
FlutLab.io <>
2022-04-06 15:48:11 +0000

    Autosave

--- lib/ui/home_2/home_presenter.dart
+++ lib/ui/home_2/home_presenter.dart
@@ -54,6 +54,7 @@ class BasicHomePresenter implements HomePresenter {
 
   @override
   Future<List> getEventListData() async {
+    await SharedPrefsHelper().setData("events", "");
     var events = await SharedPrefsHelper().getData("events");
     return events;
   }
