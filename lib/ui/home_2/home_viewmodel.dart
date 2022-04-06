6ad8ea9cc2959d197a9d9e4b75551c50a7e6f92f
FlutLab.io <>
2022-04-06 10:41:20 +0000

    Autosave

--- lib/ui/home_2/home_viewmodel.dart
+++ lib/ui/home_2/home_viewmodel.dart
@@ -1,15 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
+
 class HomeViewModel {
   List _events = [];
-  List _todayEvents = [];
-
   List get events => _events;
   set events(List newEvents) {
     _events = newEvents;
   }
 
-  List get todayEvents => _todayEvents;
-  set todayEvents(List newTodayEvents) {
-    _todayEvents = newTodayEvents;
+
+  List get todayEvents() {
+    return _events.where((_event) => _event.date.isAfter(DateTime.now().subtract(Duration(days: 1))) ).toList();
+  }
+
+  List get eventsByStatus(int eventStatus) {
+    return _events.where((_event) => _event.status == eventStatus).toList();
   }
 
   HomeViewModel();
