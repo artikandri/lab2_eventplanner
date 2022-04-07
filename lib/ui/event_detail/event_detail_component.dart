e12c58a62f444b771908bdbfc368fd521f57cc3b
FlutLab.io <>
2022-04-07 09:40:50 +0000

    Autosave

--- lib/ui/event_detail/event_detail_component.dart
+++ lib/ui/event_detail/event_detail_component.dart
@@ -86,7 +86,7 @@ class _EventDetailPageState extends State<EventDetailPage> implements EventDetai
                       ),
                     ]),
                     Spacer(),
-                    iconContainer(getTypeDataFromValue(_eventData['type']).icon, AppColors.transparent, AppColors.kDarkPurple, isBordered: true, borderColor: AppColors.kDarkPurple),
+                    iconContainer(getTypeDataFromValue(_eventData['type']).icon, getTypeDataFromValue(_eventData['type']).color, AppColors.kDarkPurple, isBordered: true, borderColor: AppColors.kDarkPurple),
                   ],
                 ),
               ],
