b218f2994a0cddf5f15e72772f9e6411cec4cee6
FlutLab.io <>
2022-04-07 01:07:08 +0000

    Autosave

--- lib/widgets/event_list_item.dart
+++ lib/widgets/event_list_item.dart
@@ -16,48 +16,50 @@ class EventListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Opacity(
         opacity: opacity,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            CircleAvatar(
-              radius: 20.0,
-              backgroundColor: eventColor == null ? AppColors.kLightGreen : eventColor,
-              child: Icon(
-                eventIcon,
-                size: AppSpacings.defaultSpacing * 2,
-                color: Colors.white,
-              ),
-            ),
-            SizedBox(width: AppSpacings.defaultSpacing * 2),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+        child: Container(
+            padding: EdgeInsets.all(AppSpacings.defaultSpacing * 2),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Text(
-                  title,
-                  style: TextStyle(
-                    fontSize: AppFonts.bodyFontSize,
-                    fontWeight: FontWeight.w400,
+                CircleAvatar(
+                  radius: 20.0,
+                  backgroundColor: eventColor == null ? AppColors.kLightGreen : eventColor,
+                  child: Icon(
+                    eventIcon,
+                    size: AppSpacings.defaultSpacing * 2,
+                    color: Colors.white,
                   ),
                 ),
-                SizedBox(height: AppSpacings.defaultSpacing),
-                Text(
-                  date,
-                  style: TextStyle(fontSize: AppFonts.subFontSize, fontWeight: FontWeight.w300, color: Colors.black45),
+                SizedBox(width: AppSpacings.defaultSpacing * 2),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: AppFonts.bodyFontSize,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    SizedBox(height: AppSpacings.defaultSpacing),
+                    Text(
+                      date,
+                      style: TextStyle(fontSize: AppFonts.subFontSize, fontWeight: FontWeight.w300, color: Colors.black45),
+                    ),
+                  ],
                 ),
+                SizedBox(width: AppSpacings.defaultSpacing * 2),
+                Column(children: <Widget>[
+                  Text(
+                    subtitle,
+                    textAlign: TextAlign.right,
+                    style: TextStyle(
+                      fontSize: AppFonts.subFontSize,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  )
+                ])
               ],
-            ),
-            SizedBox(width: AppSpacings.defaultSpacing * 2),
-            Column(children: <Widget>[
-              Text(
-                subtitle,
-                textAlign: TextAlign.right,
-                style: TextStyle(
-                  fontSize: AppFonts.subFontSize,
-                  fontWeight: FontWeight.w500,
-                ),
-              )
-            ])
-          ],
-        ));
+            )));
   }
 }
