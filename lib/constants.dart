6843626465363512349502f42bcd3e788a090f4b
FlutLab.io <>
2022-04-07 08:05:05 +0000

    Autosave

--- lib/constants.dart
+++ lib/constants.dart
@@ -44,7 +44,7 @@ List<TypeOption> typeOptions = [
 ];
 
 getTypeDataFromValue(int value) {
-  return typeOptions.where((t) => t.value == value).toList()[0];
+  return typeOptions.where((t) => t.value == value).toList();
 }
 
 IconData getStatusIconFromValue(int value) {
