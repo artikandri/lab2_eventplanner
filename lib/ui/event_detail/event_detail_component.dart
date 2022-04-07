d413ea8e4a502f57d195c9fa55db88acbd65585d
FlutLab.io <>
2022-04-07 09:30:08 +0000

    Autosave

--- lib/ui/event_detail/event_detail_component.dart
+++ lib/ui/event_detail/event_detail_component.dart
@@ -106,7 +106,7 @@ class _EventDetailPageState extends State<EventDetailPage> implements EventDetai
                 height: AppSpacings.defaultSpacing,
               ),
               Row(children: <Widget>[
-                iconContainer(Icons.calendar_month, AppColors.transparent, getTypeDataFromValue(_eventData['type']).color),
+                iconContainer(Icons.calendar, AppColors.transparent, getTypeDataFromValue(_eventData['type']).color),
                 Text(
                   _eventData['date'],
                   style: TextStyle(fontSize: AppFonts.bodyFontSize, fontWeight: FontWeight.w300),
@@ -116,7 +116,7 @@ class _EventDetailPageState extends State<EventDetailPage> implements EventDetai
                 height: AppSpacings.defaultSpacing,
               ),
               Row(children: <Widget>[
-                iconContainer(Icons.access_time, AppColors.transparent, getTypeDataFromValue(_eventData['type']).color),
+                iconContainer(Icons.time, AppColors.transparent, getTypeDataFromValue(_eventData['type']).color),
                 Text(
                   _eventData['time'],
                   style: TextStyle(fontSize: AppFonts.bodyFontSize, fontWeight: FontWeight.w300),
