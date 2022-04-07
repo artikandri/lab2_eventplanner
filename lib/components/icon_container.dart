fdad5602bdd704e7fb7ee599114f8a149acb9ea0
FlutLab.io <>
2022-04-07 09:37:38 +0000

    Autosave

--- lib/components/icon_container.dart
+++ lib/components/icon_container.dart
@@ -3,12 +3,16 @@ import 'package:eventplanner/theme/index.dart';
 
 CircleAvatar iconContainer(icon, backgroundColor, iconColor) {
   return CircleAvatar(
-    radius: AppSpacings.defaultSpacing * 3,
-    backgroundColor: backgroundColor,
-    child: Icon(
-      icon,
-      size: AppSpacings.defaultSpacing * 2,
-      color: iconColor,
+    radius: (AppSpacings.defaultSpacing * 3) + 2,
+    backgroundColor: Colors.teal,
+    child: CircleAvatar(
+      radius: AppSpacings.defaultSpacing * 3,
+      backgroundColor: backgroundColor,
+      child: Icon(
+        icon,
+        size: AppSpacings.defaultSpacing * 2,
+        color: iconColor,
+      ),
     ),
   );
 }
